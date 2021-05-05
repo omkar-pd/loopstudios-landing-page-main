e663c1fdb57d02e8ee36c282351f71392fc744f6
omkar-pd <omkarpd109@gmail.com>
2021-05-05 10:45:39 +0530

    updated script.js
    used toggle class in script to add menu

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-function myfunction () {
-    var nav  = document.getElementById("h-ul");
-if (nav.className === "hero-ul") {
-    nav.className += " active";
+// function myfunction () {
+//     var nav  = document.getElementById("h-ul");
+// if (nav.className === "hero-ul") {
+//     nav.className += " active";
    
-  } else {
-    nav.className = "hero-ul";
-      }
+//   } else {
+//     nav.className = "hero-ul";
+//       }
+// }
+function myfunction(){
+   var nav  = document.getElementById("h-ul");
+   nav.classList.toggle("active")
 }
